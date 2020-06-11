9079c2f8034ec9c18a1bc0296c4c9aa296cbe26c
Tadeusz Magura-Witkowski <tadeuszmw@gmail.com>
2020-06-11 15:19:39 +0200

    Fix CI (GD-77 build only)
    Fix CI (GD-77 build only)

--- ci/build.sh
+++ ci/build.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Copy propertiary data needed to compile this stuff
-cp /data/* firmware/linkerdata/
+cp /data/codec_bin* firmware/linkerdata/
 
 # it needs X server for whatever reason, no time to debug it now ;)
 xvfb-run /usr/local/mcuxpressoide/ide/mcuxpressoide -nosplash --launcher.suppressErrors \
         -application org.eclipse.cdt.managedbuilder.core.headlessbuild \
         -data /tmp/workspace \
         -import `pwd`/firmware \
-        -cleanBuild firmware/Debug 
+        -cleanBuild firmware/Debug_GD-77
 
-test -f firmware/Debug/firmware.sgl
+test -f firmware/Debug_GD-77/firmware.sgl
